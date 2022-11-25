6659b74d1fd7fab5e6c8e9cf2e1d13272d68fef6
ymind <ymind.chan@yi.team>
2022-11-25 10:14:26 +0800

    build(gradle:plugin): bumped com.github.ben-manes.versions version to 0.44.0

--- src/test/kotlin/team/yi/jacksync/JacksyncTest.kt
+++ src/test/kotlin/team/yi/jacksync/JacksyncTest.kt
@@ -3,6 +3,7 @@ package team.yi.jacksync
 import org.junit.jupiter.api.*
 import team.yi.jacksync.support.dto.*
 import team.yi.jacksync.sync.SyncObject
+import java.util.*
 
 class JacksyncTest : BaseTest() {
     private lateinit var jacksync: Jacksync
@@ -51,5 +52,37 @@ class JacksyncTest : BaseTest() {
         val syncPostV2 = jacksync.syncProcessor.clientSync(SyncObject(1L, postV1), syncData)
 
         Assertions.assertEquals(syncPostV2.data, postV2)
+
+        println(0.toBitSet().also {
+            it.set(0, true)
+            it.set(1, true)
+            it.set(2, true)
+        }.toInt())
+    }
+}
+
+fun Int.toBitSet(): BitSet {
+    var intValue = this
+    val bits = BitSet()
+    var index = 0
+
+    while (intValue != 0) {
+        if (intValue % 2 != 0) bits.set(index)
+
+        ++index
+
+        intValue = intValue ushr 1
+    }
+
+    return bits
+}
+
+fun BitSet.toInt(): Int {
+    return (0 until this.length()).sumOf {
+        if (this[it]) {
+            1 shl it
+        } else {
+            0
+        }
     }
 }
