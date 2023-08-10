e3ae7adcff383b47bd3576cd522aefb26a9b9f12
ymind <ymind.chan@yi.team>
2023-08-10 10:01:46 +0800

    chore(deps): bumped junit-jupiter-engine version to 5.10.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
 
     // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter-engine/
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.0")
 
     // detekt
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:${detekt.toolVersion}")
