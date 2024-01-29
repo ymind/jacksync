f08f76dfe38c95b17ed57ed7ca67d9c9442677fa
ymind <ymind.chan@yi.team>
2024-01-29 17:24:37 +0800

    chore(deps): bumped jackson-databind version to 2.16.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
 
     // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter-engine/
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.0")
